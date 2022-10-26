5e039677ffe48ef41977e5ef104879e68468dcb0
kotiktom <kotikangastommi@hotmail.com>
2022-10-26 23:30:45 +0300

    Set environment in deployment as not CI

--- src/pages/week.js
+++ src/pages/week.js
@@ -47,7 +47,6 @@ const Week = () => {
 
   React.useEffect(() => {
     fetchData(guilds);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function getNext4MonthsEvents() {
