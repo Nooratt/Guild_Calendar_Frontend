0a84cc54afad52ffcd2ba3f04fbcff8b04d10226
Burkova Anastasiia <anastasiia.burkova@aalto.fi>
2022-11-21 14:47:28 +0200

    Updated filters

--- src/pages/month.js
+++ src/pages/month.js
@@ -1,167 +1,184 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import momentTimezonePlugin from '@fullcalendar/moment-timezone';
-import swal from 'sweetalert';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import momentTimezonePlugin from "@fullcalendar/moment-timezone";
+import swal from "sweetalert";
+import Select from "react-select";
 
 const Month = () => {
+  const [error, setError] = React.useState(false);
+  const [response, setResponse] = React.useState([]);
+  const guilds = [
+    "AK",
+    "AS",
+    "Athene",
+    "IK",
+    "Inkubio",
+    "KIK",
+    "MK",
+    "PJK",
+    "PT",
+    "TIK",
+    "TF",
+    "Prodeko",
+    "FK",
+  ];
+  const [filtered, setFiltered] = React.useState([]);
 
-    const [error, setError] = React.useState(false);
-    const [response, setResponse] = React.useState([]);
-    const guilds = ['AK', 'AS', 'Athene', 'IK', 'Inkubio', 'KIK', 'MK', 'PJK', 'PT', 'TIK', 'TF', 'Prodeko', 'FK'];
-    const [filtered, setFiltered] = React.useState([]);
-    const [checkedState, setCheckedState] = React.useState(new Array(guilds.length).fill(true));
+  const guildOptions = [
+    { value: "AS", label: "AS" },
+    { value: "AK", label: "AK" },
+    { value: "Athene", label: "Athene" },
+    { value: "IK", label: "IK" },
+    { value: "Inkubio", label: "Inkubio" },
+    { value: "KIK", label: "KIK" },
+    { value: "MK", label: "MK" },
+    { value: "PJK", label: "PJK" },
+    { value: "PT", label: "PT" },
+    { value: "TIK", label: "TIK" },
+    { value: "TF", label: "TF" },
+    { value: "Prodeko", label: "Prodeko" },
+    { value: "FK", label: "FK" },
+  ];
 
-    const guildQuery = guilds.map(g => `guildNames=${g}`).join('&');
-    const startDateTimeQuery = `startDateTimeFrame=${getOneMonthFromNowEvents()}`;
-    const endDateTimeQuery = `endDateTimeFrame=${getNext3MonthsEvents()}`;
-    async function fetchData() {
-        try {
-            await Promise.all([
-                (
-                    await fetch(`https://apim-whatsthehaps.azure-api.net/v1/events?${guildQuery}&${startDateTimeQuery}&${endDateTimeQuery}`, {
-                        method: 'GET',
-                        headers: { 'Content-type': 'application/json; charset=UTF-8' },
-                    }).then((res) => res.json())
-                        .then((data) => {
-                            var dataString = JSON.stringify(data.response);
-                            var result = JSON.parse(dataString);
-                            var array = [];
-                            for (let i of result) {
-                                array = array.concat(Object.values(i)[0]);
-                            }
-                            console.log("array ", array);
-                            setResponse(array);
-                            setFiltered(array);
-                        }))
-            ]);
-        } catch (error) {
-            console.log(error);
-            setError(true);
-        }
-    }
-
-    React.useEffect(() => {
-        fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    function getNext3MonthsEvents() {
-        var date = new Date();
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        date.setMonth(date.getMonth() + 3);
-        return date.toISOString();
-    }
-
-    function display3MonthsAfter() {
-        const setTo1Day = new Date(setTo1DayOfMonth());
-        setTo1Day.setMonth(setTo1Day.getMonth() + 3);
-        return setTo1Day.toISOString();
-    }
-
-    function setTo1DayOfMonth() {
-        const currentDate = new Date();
-        const setTo1DayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
-        return setTo1DayOfMonth.toISOString();
-
-    }
-
-    function getOneMonthFromNowEvents() {
-        var date = new Date();
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        date.setDate(date.getDate() - 31);
-        return date.toISOString();
-    }
-
-    if (error) {
-        return (
-            <h1> Oops.. something went wrong! </h1>
+  const guildQuery = guilds.map((g) => `guildNames=${g}`).join("&");
+  const startDateTimeQuery = `startDateTimeFrame=${getOneMonthFromNowEvents()}`;
+  const endDateTimeQuery = `endDateTimeFrame=${getNext3MonthsEvents()}`;
+  async function fetchData() {
+    try {
+      await Promise.all([
+        await fetch(
+          `https://apim-whatsthehaps.azure-api.net/v1/events?${guildQuery}&${startDateTimeQuery}&${endDateTimeQuery}`,
+          {
+            method: "GET",
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+          }
         )
+          .then((res) => res.json())
+          .then((data) => {
+            var dataString = JSON.stringify(data.response);
+            var result = JSON.parse(dataString);
+            var array = [];
+            for (let i of result) {
+              array = array.concat(Object.values(i)[0]);
+            }
+            console.log("array ", array);
+            setResponse(array);
+            setFiltered(array);
+          }),
+      ]);
+    } catch (error) {
+      console.log(error);
+      setError(true);
     }
+  }
 
-    const handleChange = (position, name) => {
-        const updatedCheckedState = checkedState.map((item, index) =>
-            index === position ? !item : item
-        );
+  React.useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        setCheckedState(updatedCheckedState);
+  function getNext3MonthsEvents() {
+    var date = new Date();
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    date.setMonth(date.getMonth() + 3);
+    return date.toISOString();
+  }
 
-        if (checkedState[position] === true) {
-            const filteredEvents = filtered.filter(value => value.guild !== name);
-            setFiltered(filteredEvents);
-        } else {
-            const toBeAppliedEvents = response.filter(x => x.guild === name);
-            const applied = [...filtered, ...toBeAppliedEvents];
-            setFiltered(applied);
-        }
-    };
+  function display3MonthsAfter() {
+    const setTo1Day = new Date(setTo1DayOfMonth());
+    setTo1Day.setMonth(setTo1Day.getMonth() + 3);
+    return setTo1Day.toISOString();
+  }
 
-    return (
-
-        <div className='container2'>
-            <div>
-                <ul className="sorting-list">
-                    {guilds.map((name, index) => {
-                        return (
-                            <li key={index}>
-                                <div className="sorting-list-item">
-                                    <div>
-                                        <input
-                                            type="checkbox"
-                                            id={`custom-checkbox-${index}`}
-                                            name={name}
-                                            value={name}
-                                            checked={checkedState[index]}
-                                            onChange={() => handleChange(index, name)}
-                                        />
-                                        <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
-                                    </div>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-            <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, momentTimezonePlugin]}
-                initialView="dayGridMonth"
-                eventTimeFormat={{ hour: '2-digit', minute: '2-digit', hour12: false }}
-                firstDay={1}
-                validRange={{
-                    start: setTo1DayOfMonth(),
-                    end: display3MonthsAfter()
-                }}
-                fixedWeekCount={false}
-                eventOverlap={false}
-                events={filtered}
-                height={"auto"}
-                eventDisplay={"list-item"}
-                dayMaxEvents={3}
-                nowIndicator={true}
-                eventClick={(e) => {
-                    swal({
-                      title: e.event.title,
-                      text: "Starting from: " + e.event.start +
-                      "\n Description: " + e.event.extendedProps.description +
-                      "\n Location: " + e.event.extendedProps.location +
-                      "\n Organizer: " + e.event.extendedProps.guild
-                    })               
-                  }}
-                timeZone="Europe/Helsinki"
-            />
-
-        </div>
+  function setTo1DayOfMonth() {
+    const currentDate = new Date();
+    const setTo1DayOfMonth = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      1
     );
+    return setTo1DayOfMonth.toISOString();
+  }
 
+  function getOneMonthFromNowEvents() {
+    var date = new Date();
+    date.setMinutes(0);
+    date.setSeconds(0);
+    date.setMilliseconds(0);
+    date.setDate(date.getDate() - 31);
+    return date.toISOString();
+  }
 
+  if (error) {
+    return <h1> Oops.. something went wrong! </h1>;
+  }
 
+  const handleChange = (values) => {
+    var filteredEvents = [];
+    var applied = [];
+    for (let i in values) {
+      filteredEvents = response.filter(
+        (value) => value.guild === values[i].value
+      );
+      applied = [...applied, ...filteredEvents];
+    }
+    setFiltered(applied);
+  };
+
+  return (
+    <div className="container2">
+      <Select
+        isMulti
+        name="colors"
+        options={guildOptions}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        onChange={handleChange}
+      />
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          momentTimezonePlugin,
+        ]}
+        initialView="dayGridMonth"
+        eventTimeFormat={{ hour: "2-digit", minute: "2-digit", hour12: false }}
+        firstDay={1}
+        validRange={{
+          start: setTo1DayOfMonth(),
+          end: display3MonthsAfter(),
+        }}
+        fixedWeekCount={false}
+        eventOverlap={false}
+        events={filtered}
+        height={"auto"}
+        eventDisplay={"list-item"}
+        dayMaxEvents={3}
+        nowIndicator={true}
+        eventClick={(e) => {
+          swal({
+            title: e.event.title,
+            text:
+              "Starting from: " +
+              e.event.start +
+              "\n Description: " +
+              e.event.extendedProps.description +
+              "\n Location: " +
+              e.event.extendedProps.location +
+              "\n Organizer: " +
+              e.event.extendedProps.guild,
+          });
+        }}
+        timeZone="Europe/Helsinki"
+      />
+    </div>
+  );
 };
-
 
 export default Month;
--- src/pages/week.js
+++ src/pages/week.js
@@ -1,41 +1,73 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import momentTimezonePlugin from '@fullcalendar/moment-timezone';
-import '../components/Navbar/NavStyles.css';
-import Swal from 'sweetalert2';
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import momentTimezonePlugin from "@fullcalendar/moment-timezone";
+import "../components/Navbar/NavStyles.css";
+import Swal from "sweetalert2";
+import Select from "react-select";
 
 const Week = () => {
-
   const [error, setError] = React.useState(false);
   const [response, setResponse] = React.useState([]);
-  const guilds = ['AK', 'AS', 'Athene', 'IK', 'Inkubio', 'KIK', 'MK', 'PJK', 'PT', 'TIK', 'TF', 'Prodeko', 'FK'];
+  const guilds = [
+    "AK",
+    "AS",
+    "Athene",
+    "IK",
+    "Inkubio",
+    "KIK",
+    "MK",
+    "PJK",
+    "PT",
+    "TIK",
+    "TF",
+    "Prodeko",
+    "FK",
+  ];
   const [filtered, setFiltered] = React.useState([]);
-  const [checkedState, setCheckedState] = React.useState(new Array(guilds.length).fill(true));
 
-  const guildQuery = guilds.map(g => `guildNames=${g}`).join('&');
+  const guildOptions = [
+    { value: "AS", label: "AS" },
+    { value: "AK", label: "AK" },
+    { value: "Athene", label: "Athene" },
+    { value: "IK", label: "IK" },
+    { value: "Inkubio", label: "Inkubio" },
+    { value: "KIK", label: "KIK" },
+    { value: "MK", label: "MK" },
+    { value: "PJK", label: "PJK" },
+    { value: "PT", label: "PT" },
+    { value: "TIK", label: "TIK" },
+    { value: "TF", label: "TF" },
+    { value: "Prodeko", label: "Prodeko" },
+    { value: "FK", label: "FK" },
+  ];
+
+  const guildQuery = guilds.map((g) => `guildNames=${g}`).join("&");
   const startDateTimeQuery = `startDateTimeFrame=${get10DaysFromNowEvents()}`;
   const endDateTimeQuery = `endDateTimeFrame=${getNext4MonthsEvents()}`;
   async function fetchData(guilds) {
     try {
       await Promise.all([
-        (
-          await fetch(`https://apim-whatsthehaps.azure-api.net/v1/events?${guildQuery}&${startDateTimeQuery}&${endDateTimeQuery}`, {
-            method: 'GET',
-            headers: { 'Content-type': 'application/json; charset=UTF-8' },
-          }).then((res) => res.json())
-            .then((data) => {
-              var dataString = JSON.stringify(data.response);
-              var result = JSON.parse(dataString);
-              var array = [];
-              for (let i of result) {
-                array = array.concat(Object.values(i)[0]);
-              }
-              setResponse(array);
-              setFiltered(array);
-            }))
+        await fetch(
+          `https://apim-whatsthehaps.azure-api.net/v1/events?${guildQuery}&${startDateTimeQuery}&${endDateTimeQuery}`,
+          {
+            method: "GET",
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+          }
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            var dataString = JSON.stringify(data.response);
+            var result = JSON.parse(dataString);
+            var array = [];
+            for (let i of result) {
+              array = array.concat(Object.values(i)[0]);
+            }
+            setResponse(array);
+            setFiltered(array);
+          }),
       ]);
     } catch (error) {
       console.log(error);
@@ -68,116 +100,86 @@ const Week = () => {
 
   function setToMonday(date) {
     var day = date.getDay() || 7;
-    if (day !== 1)
-      date.setHours(-24 * (day - 1));
+    if (day !== 1) date.setHours(-24 * (day - 1));
     return date;
   }
 
   if (error) {
-    return (
-      <h1> Oops.. something went wrong! </h1>
-    )
+    return <h1> Oops.. something went wrong! </h1>;
   }
 
-  function handleChange(position, name) {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-
-    setCheckedState(updatedCheckedState);
-
-    if (checkedState[position] === true) {
-      const filteredEvents = filtered.filter(value => value.guild !== name);
-      setFiltered(filteredEvents);
-    } else {
-      const toBeAppliedEvents = response.filter(x => x.guild === name);
-      const applied = [...filtered, ...toBeAppliedEvents];
-      setFiltered(applied);
+  const handleChange = (values) => {
+    var filteredEvents = [];
+    var applied = [];
+    for (let i in values) {
+      filteredEvents = response.filter(
+        (value) => value.guild === values[i].value
+      );
+      applied = [...applied, ...filteredEvents];
     }
+    setFiltered(applied);
   };
 
   return (
-
-    <div className='container2'>
-      
+    <div className="container2">
+      <Select
+        defaultValue={[guildOptions[guildOptions.length]]}
+        isMulti
+        name="colors"
+        options={guildOptions}
+        className="basic-multi-select"
+        classNamePrefix="select"
+        onChange={handleChange}
+      />
       <FullCalendar
-        customButtons={{
-          filterDropdown: {
-            text: 'Filter',
-            click: function(){
-              Swal.fire({
-                title: "Filter events by guild",
-                html:'<div>'+
-                '<ul className="sorting-list" style="list-style: none;">'+ guilds.map((name, index) => {
-                    return (
-                      '<li key='+ index +'>'+
-                        '<div className="sorting-list-item">'+
-                          '<div>'+
-                            '<input'+
-                              ' type="checkbox"'+
-                              ' id={custom-checkbox-' + index +'}'+
-                              ' name='+name+
-                              ' value='+name+
-                              ' checked='+checkedState[index]+
-                              ' onChange=' + handleChange(index, name)+
-                    
-                            '/>'+
-                            '<label htmlFor={`custom-checkbox-' + index +'}>'+name+'</label>'+
-                          '</div>'+
-                        '</div>'+
-                      '</li>'
-                    );
-                  }) +
-                '</ul>'+
-              '</div>'
-              })
-            }
-        }
-        }}
-        headerToolbar={{start: 'title',end:'filterDropdown today prev,next'}}
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, momentTimezonePlugin]}
+        headerToolbar={{ start: "title", end: "today prev,next" }}
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          momentTimezonePlugin,
+        ]}
         initialView="timeGridWeek"
         slotMinTime={"08:00:00"}
         slotMaxTime={"24:00:00"}
-        eventTimeFormat={{ hour: '2-digit', minute: '2-digit', hour12: false }}
-        eventMinHeight='50'
+        eventTimeFormat={{ hour: "2-digit", minute: "2-digit", hour12: false }}
+        eventMinHeight="50"
         dayMaxEventRows={3}
         firstDay={1}
         weekNumbers={true}
         weekText={"Week"}
-        dayHeaderFormat={{ weekday: 'short', day: 'numeric', month: 'numeric' }}
+        dayHeaderFormat={{ weekday: "short", day: "numeric", month: "numeric" }}
         validRange={{
           start: setToMonday(new Date()),
-          end: setToMonday(new Date(getNext4MonthsEvents())).toISOString()
+          end: setToMonday(new Date(getNext4MonthsEvents())).toISOString(),
         }}
         eventOverlap={false}
-        //headerToolbar={false}
-        //dayHeaders={false}
-      
+
         dayHeaderClassNames={"weekDay"}
         events={filtered}
         eventDisplay={"block"}
-        
-
         nowIndicator={true}
         eventClick={(e) => {
           Swal.fire({
             title: e.event.title,
-            html: "Starting from: " + e.event.start+ "<br>"+
-            "<br>Description: " + e.event.extendedProps.description + "<br>"+ 
-            "<br> Location: " + e.event.extendedProps.location + "<br>"+
-            "<br>Organizer: " + e.event.extendedProps.guild
-          })               
+            html:
+              "Starting from: " +
+              e.event.start +
+              "<br>" +
+              "<br>Description: " +
+              e.event.extendedProps.description +
+              "<br>" +
+              "<br> Location: " +
+              e.event.extendedProps.location +
+              "<br>" +
+              "<br>Organizer: " +
+              e.event.extendedProps.guild,
+          });
         }}
         timeZone="Europe/Helsinki"
       />
-
     </div>
   );
-
-
-
 };
-
 
 export default Week;
